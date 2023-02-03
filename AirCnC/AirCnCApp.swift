8756c1b172228386102efe998aa54ad75b54307c
i1hwan <mtslzx@gmail.com>
2023-02-03 20:19:49 +0900

    Initial Commit

---
+++ AirCnC/AirCnCApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirCnCApp.swift
+//  AirCnC
+//
+//  Created by 하일환 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct AirCnCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
